cb0e09b989524906cc98034e5ea18cb9b2fb295a
Lucas Medina dos Santos <lucas.santos@webmotors.com.br>
2024-12-04 15:31:01 -0300

    Adds vite example with styled-components + babel options

---
+++ vite-with-styled-components/src/components/Toggler/index.jsx
@@ -0,0 +1,9 @@
+import { Title } from './styles'
+
+export default function Toggler() {
+    return (
+        <Title>Oh no! A donkey-rabbit!</Title>
+    )
+}
+
+Toggler.displayName = "MyTogglerComponent"
---
+++ vite-with-styled-components/src/components/Toggler/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+    font-family: Verdana;
+    font-size: 42px;
+    color: red;
+`
